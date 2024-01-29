970a7bb19866aa27e0e49423c38e1e9edd05e471
Ykov91 <yakov.litvinenko.91@mail.ru>
2024-01-30 00:13:40 +0900

    Commit 1

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,13 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    private String death;
+
+    @Override
+    public String toString() {
+        return "Death Koschei";
+    }
 }
